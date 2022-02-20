498cdfd2d866b34e95ac053981732e5158ceb8ad
Jakub Chromec <chromec.jakub@gmail.com>
2022-02-20 17:37:45 +0100

    tweak docs

--- R/fetch.R
+++ R/fetch.R
@@ -1,9 +1,15 @@
-#' Perform A Scraper Request
+#' Issue A Scrape Request
 #'
-#' Download a CSFD page and return a scraper object.
+#' Download a single CSFD page and return a scraper object. Rate is throttled
+#' at 10 requests per minute.
 #'
-#' @param url A CSFD URL, even a partial one, e.g. `"/film/9834-star-trek-film"`
+#' @param url A CSFD URL, incl. a partial one, e.g. `"/film/9834-star-trek-film"`
 #' @param quiet If `TRUE`, request status will not be printed on the console.
+#'
+#'   This indicates whether the request is being executed or has been completed
+#'   and with what status. It also prints out the response URL, which may be
+#'   different from the request URL, because the server always returns a complete
+#'   URL rather than its short version (e.g. `tvurce/453`).
 #'
 #' @return An object of class [csfd_scraper].
 #' @export
